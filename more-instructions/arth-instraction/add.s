840e93f1cdf7afb1cda4c89c5f3d534e992b2e61
hedi-ben-maouii <teedydeev@gmail.com>
2024-04-14 18:07:22 +0100

    add comments

--- more-instructions/arth-instraction/add.s
+++ more-instructions/arth-instraction/add.s
@@ -7,9 +7,8 @@ global _start
 
 _start:
         MOV al,0b11111111
-        MOV bl,0b0001
+        MOV bl,0b0001 
         ADD bl,al 
-        
-        ADC bh,0 // Add wiht the carry in eflags register if it has one 
+        ADC bh,0 ; Add wiht the carry in eflags register if it has one 
         
         INT 80h
--- more-instructions/logic-operator/shift.s
+++ more-instructions/logic-operator/shift.s
@@ -5,8 +5,8 @@ global _start
 
 _start:
         MOV eax,0b0010
-        SHR eax,1   // 1010 -> 0101  like devide by 2 
-        SHL eax,2   // 1010 <- 0100  like mult by 2 
-        SAL eax,1   // 1010 -> 1101  same as devide by 2 but with sign value 
-        SAL eax,2   // 1010 <- 1100  same as mult by 2 but with sign value
+        SHR eax,1   ; 1010 -> 0101  like devide by 2 
+        SHL eax,2   ; 1010 <- 0100  like mult by 2 
+        SAL eax,1   ; 1010 -> 1101  same as devide by 2 but with sign value 
+        SAL eax,2   ; 1010 <- 1100  same as mult by 2 but with sign value
         INT 80h 
